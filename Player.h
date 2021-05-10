2b40b26e18716a0b4a5895e4afa78e82038b0207
homelessalmon <83202278+homelessalmon@users.noreply.github.com>
2021-05-10 14:14:09 +0800

    Update Player.h

--- Player.h
+++ Player.h
@@ -1,5 +1,13 @@
+//Player.h v0
 #pragma once
+#include "Chess.h"
+
 class Player
 {
+	King king;
+	Queen queen;
+	Rook rook1, rook2;
+	Bishop bishop1, bishop2;
+	Knight knight1, knight2;
+	Pawn pawn1, pawn2, pawn3, pawn4, pawn5, pawn6, pawn7, pawn8;
 };
-
