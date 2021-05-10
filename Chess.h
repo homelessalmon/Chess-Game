570533b3a9c6c972c01832b929ece679ed578473
homelessalmon <83202278+homelessalmon@users.noreply.github.com>
2021-05-10 14:14:44 +0800

    Update Chess.h

--- Chess.h
+++ Chess.h
@@ -1,7 +1,39 @@
+//chess.h v0
 #pragma once
+
 class Chess
 {
 public:
 	int i, j;
+
+};
+
+class King : public Chess
+{
+
+};
+
+class Queen : public Chess
+{
+
+};
+
+class Rook : public Chess
+{
+
+};
+
+class Bishop : public Chess
+{
+
+};
+
+class Knight : public Chess
+{
+
+};
+
+class Pawn : public Chess
+{
 
 };
