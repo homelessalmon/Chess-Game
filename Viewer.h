7ecb68ab6a518d130349c7fc664d960b9378f17f
shinku2846 <shinku2846@gmail.com>
2021-05-13 04:47:13 +0800

    Update Viewer.h

--- Viewer.h
+++ Viewer.h
@@ -1,5 +1,21 @@
 #pragma once
-class Viewer
-{
-};
+#include <opencv2/opencv.hpp>
+#include "ChessPiece.h"
+#define SIZE 50
+using namespace std;
+using namespace cv;
+typedef class ChessPiece ChessPiece;
 
+class Viewer {
+public:
+	Viewer();
+	void drawBoard();
+	void drawChess(ChessPiece piece);
+	void drawMovable(ChessPiece piece);
+	Mat Screen;
+private:
+	Point BoradtoImg(int x, int y);
+	int ImgtoBoradX(Point P);
+	int ImgtoBoradY(Point P);
+	bool mergeImg(Mat& dst, Mat& src, Point location);
+};
