965a2432737416b0eb3c472a86a4df6ef24b96c3
shinku2846 <shinku2846@gmail.com>
2021-05-13 04:46:43 +0800

    Update GameManager.cpp

--- GameManager.cpp
+++ GameManager.cpp
@@ -1,1 +1,17 @@
 #include "GameManager.h"
+#include <vector>
+#include <opencv2/opencv.hpp>
+#include "ChessPiece.h"
+using namespace std;
+using namespace cv;
+
+void GameManager::exe() {
+	viewer.drawBoard();
+	ChessPiece A(1, Queen, 5, 4);
+	testPiece.push_back(A);
+	for (int i = 0; i < testPiece.size(); i++) {
+		viewer.drawChess(testPiece[i]);
+	}
+	namedWindow("Chess Game", WINDOW_AUTOSIZE);
+	imshow("Chess Game", viewer.Screen);
+}
