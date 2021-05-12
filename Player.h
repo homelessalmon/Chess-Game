e5ee78394d4dfe56f0f8df81e661c983809a7fad
shinku2846 <shinku2846@gmail.com>
2021-05-13 04:46:59 +0800

    Update Player.h

--- Player.h
+++ Player.h
@@ -1,14 +1,10 @@
-//Player.h v0
 #pragma once
-#include "Chess.h"
+#include <vector>
+#include "ChessPiece.h"
+using namespace std;
+typedef class ChessPiece ChessPiece;
 
-class Player
-{
-public:
-	King king;
-	Queen queen;
-	Rook rook1, rook2;
-	Bishop bishop1, bishop2;
-	Knight knight1, knight2;
-	Pawn pawn1, pawn2, pawn3, pawn4, pawn5, pawn6, pawn7, pawn8;
+class Player {
+	vector<ChessPiece> OwningPiece;
+
 };
