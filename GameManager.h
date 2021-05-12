95e2398902644bc0f79149e72007784c9277109d
shinku2846 <shinku2846@gmail.com>
2021-05-13 04:46:29 +0800

    Update GameManager.h

--- GameManager.h
+++ GameManager.h
@@ -1,4 +1,22 @@
 #pragma once
-class GameManager
-{
+#include <vector>
+#include "Player.h"
+#include "Viewer.h"
+#include "ChessPiece.h"
+using namespace std;
+typedef class ChessPiece ChessPiece;
+#define TEST 1
+
+class GameManager {
+public:
+	Player* players[2];
+	int current_player;
+	Viewer viewer;
+	void exe();
+
+#if TEST == 1
+	vector<ChessPiece> testPiece;
+
+#endif // TEST == 1
+
 };
