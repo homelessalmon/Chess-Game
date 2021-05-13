3dd3761cfdaaf7db7b161efac5e3df9a9198a700
shinku2846 <shinku2846@gmail.com>
2021-05-14 05:28:27 +0800

    Update GameManager.h

--- GameManager.h
+++ GameManager.h
@@ -12,11 +12,21 @@ public:
 	Player* players[2];
 	int current_player;
 	Viewer viewer;
+	// int boardSituation[8][8]; //可能會用到？
+
+
 	void exe();
+	static void mouseCallback1(int event, int x, int y, int flags, void* param);
+	static void mouseCallback2(int event, int x, int y, int flags, void* param);
+	void doMouseCallback1(int event, int x, int y, int flags);
+	void doMouseCallback2(int event, int x, int y, int flags);
 
 #if TEST == 1
+	int status;
+	int pieceNo;
 	vector<ChessPiece> testPiece;
 
 #endif // TEST == 1
-
+	static int ImgtoBoradX(Point P); //得出P點對應的棋盤X座標
+	static int ImgtoBoradY(Point P); //得出P點對應的棋盤Y座標
 };
