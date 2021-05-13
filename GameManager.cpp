1a680f3a59619a39dfb593a8af0c387bf9b13e66
shinku2846 <shinku2846@gmail.com>
2021-05-14 05:28:16 +0800

    Update GameManager.cpp

--- GameManager.cpp
+++ GameManager.cpp
@@ -5,7 +5,68 @@
 using namespace std;
 using namespace cv;
 
+int GameManager::ImgtoBoradX(Point P) {
+	return (P.x / SIZE - 1);
+}
+
+int GameManager::ImgtoBoradY(Point P) {
+	return -(P.y / SIZE - 8);
+}
+
+void GameManager::doMouseCallback2(int event, int x, int y, int flags) {
+	if (event == EVENT_LBUTTONUP) {
+		Point PointStart = Point(x, y);
+		int clickedX = GameManager::ImgtoBoradX(PointStart);
+		int clickedY = GameManager::ImgtoBoradY(PointStart);
+		if (testPiece[pieceNo].move(clickedX, clickedY)) {
+			viewer.drawBoard();
+			for (int i = 0; i < testPiece.size(); i++) {
+				viewer.drawChess(testPiece[i]);
+			}
+			imshow("Chess Game", viewer.Screen);
+			status = 0;
+		}
+		else {
+			viewer.drawBoard();
+			for (int i = 0; i < testPiece.size(); i++) {
+				viewer.drawChess(testPiece[i]);
+			}
+			imshow("Chess Game", viewer.Screen);
+			status = 0;
+		}
+	}
+}
+
+void GameManager::mouseCallback2(int event, int x, int y, int flags, void* param) {
+	GameManager* self = static_cast<GameManager*>(param);
+	self->doMouseCallback2(event, x, y, flags);
+}
+
+void GameManager::doMouseCallback1(int event, int x, int y, int flags) {
+	if (event == EVENT_LBUTTONUP) {
+		Point PointStart = Point(x, y);
+		int clickedX = ImgtoBoradX(PointStart);
+		int clickedY = ImgtoBoradY(PointStart);
+		for (int i = 0; i < testPiece.size(); i++) {
+			if (testPiece[i].posX == clickedX && testPiece[i].posY == clickedY) {
+				viewer.drawMovable(testPiece[i]);
+				imshow("Chess Game", viewer.Screen);
+				status = 1;
+				pieceNo = i;
+			}
+		}
+	}
+}
+
+
+
+void GameManager::mouseCallback1(int event, int x, int y, int flags, void* param) {
+	GameManager* self = static_cast<GameManager*>(param);
+	self->doMouseCallback1(event, x, y, flags);
+}
+
 void GameManager::exe() {
+	testPiece.clear();
 	viewer.drawBoard();
 	ChessPiece A(1, Queen, 5, 4);
 	testPiece.push_back(A);
@@ -14,5 +75,15 @@ void GameManager::exe() {
 	}
 	namedWindow("Chess Game", WINDOW_AUTOSIZE);
 	imshow("Chess Game", viewer.Screen);
-	setMouseCallback("Chess Game", Viewer::mouseCallback, 0);
+	status = 0;
+	while (1) {
+		if (status == 0) {
+			setMouseCallback("Chess Game", mouseCallback1, this);
+		}
+		else {
+			setMouseCallback("Chess Game", mouseCallback2, this);
+		}
+		waitKey(100);
+	}
+
 }
