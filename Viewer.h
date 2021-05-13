05c6b0415b08b60e87c09ce66f6a36e7551b22b2
shinku2846 <shinku2846@gmail.com>
2021-05-13 17:27:33 +0800

    Update Viewer.h

--- Viewer.h
+++ Viewer.h
@@ -9,15 +9,15 @@ typedef class ChessPiece ChessPiece;
 class Viewer {
 public:
 	Viewer();
-	void drawBoard();
-	void drawChess(ChessPiece piece);
-	void drawMovable(ChessPiece piece);
+	void drawBoard(); //繪製棋盤、底板
+	void drawChess(ChessPiece piece); //繪製piece對應的棋子
+	void drawMovable(ChessPiece piece); //繪製piece可走的方格
 	Mat Screen;
-	static void mouseCallback(int event, int x, int y, int flags, void *param);
+	static void mouseCallback(int event, int x, int y, int flags, void* param);
 	void doMouseCallback(int event, int x, int y, int flags);
 private:
-	Point BoradtoImg(int x, int y);
-	int ImgtoBoradX(Point P);
-	int ImgtoBoradY(Point P);
+	Point BoradtoImg(int x, int y); //得出某棋盤格左上角的座標
+	int ImgtoBoradX(Point P); //得出P點對應的棋盤X座標
+	int ImgtoBoradY(Point P); //得出P點對應的棋盤Y座標
 	bool mergeImg(Mat& dst, Mat& src, Point location);
 };
