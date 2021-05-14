f9f139aa70dd24c53c80de00930b301f3efbcb8d
kaler8234 <61696147+kaler8234@users.noreply.github.com>
2021-05-14 19:13:19 +0800

    Update Player.h

--- Player.h
+++ Player.h
@@ -1,10 +1,19 @@
 #pragma once
 #include <vector>
 #include "ChessPiece.h"
+#include<string>
+
 using namespace std;
+
 typedef class ChessPiece ChessPiece;
 
 class Player {
 	vector<ChessPiece> OwningPiece;
+public:
+	Player(int);
+};
+
+class AI_player : public Player
+{
 
 };
