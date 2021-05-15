8c13d1e79249280ebeb869f6264477659e6efc85
homelessalmon <83202278+homelessalmon@users.noreply.github.com>
2021-05-16 00:05:15 +0800

    Update ChessPiece.cpp

--- ChessPiece.cpp
+++ ChessPiece.cpp
@@ -40,15 +40,100 @@ void ChessPiece::checkMovable() {
 		//todo 檢查可動位置是否有棋子
 		break;
 	case Rook:
+		for (int faceX = -1; faceX <= 1; faceX++) {
+			for (int faceY = -1; faceY <= 1; faceY++) {
+				if (faceX != 0 && faceY != 0 || faceX == 0 && faceY == 0) {
+					continue;
+				}
+				for (int i = 1; i <= 8; i++) {
+					int targetX, targetY;
+					targetX = posX + i * faceX;
+					targetY = posY + i * faceY;
+					if (targetX >= 0 && targetX < 8 && targetY >= 0 && targetY < 8) {
+						movableX.push_back(targetX);
+						movableY.push_back(targetY);
+					}
+				}
+			}
+		}
 		// todo
 		break;
 	case Bishop:
+		for (int faceX = -1; faceX <= 1; faceX++) {
+			for (int faceY = -1; faceY <= 1; faceY++) {
+				if (faceX == 0 || faceY == 0) {
+					continue;
+				}
+				for (int i = 1; i <= 8; i++) {
+					int targetX, targetY;
+					targetX = posX + i * faceX;
+					targetY = posY + i * faceY;
+					if (targetX >= 0 && targetX < 8 && targetY >= 0 && targetY < 8) {
+						movableX.push_back(targetX);
+						movableY.push_back(targetY);
+					}
+				}
+			}
+		}
 		// todo
 		break;
 	case Knight:
+		for (int faceX = -1; faceX <= 1; faceX++) {
+			for (int faceY = -1; faceY <= 1; faceY++) {
+				if (faceX != 0 && faceY != 0 || faceX == 0 && faceY == 0) {
+					continue;
+				}
+				if (faceX == 1 || faceX == -1) {
+					for (int i = -1; i <= 1; i++) {
+						if (i == 0) continue;
+						int targetX, targetY;
+						targetX = posX + 2 * faceX;
+						targetY = posY + i * faceX;
+						if (targetX >= 0 && targetX < 8 && targetY >= 0 && targetY < 8) {
+							movableX.push_back(targetX);
+							movableY.push_back(targetY);
+						}
+					}
+				}
+				if (faceY == 1 || faceY == -1) {
+					for (int i = -1; i <= 1; i++) {
+						if (i == 0) continue;
+						int targetX, targetY;
+						targetX = posX + i * faceY;
+						targetY = posY + 2 * faceY;
+						if (targetX >= 0 && targetX < 8 && targetY >= 0 && targetY < 8) {
+							movableX.push_back(targetX);
+							movableY.push_back(targetY);
+						}
+					}
+				}
+			}
+		}
 		// todo
 		break;
 	case Pawn:
+		if (player == 0) {
+			for (int i = 1; i <= 2; i++) {
+				int targetX, targetY;
+				targetX = posX;
+				targetY = posY + i;
+				if (targetX >= 0 && targetX < 8 && targetY >= 0 && targetY < 8) {
+					movableX.push_back(targetX);
+					movableY.push_back(targetY);
+				}
+			}
+		}
+		if (player == 1) {
+			for (int i = 1; i <= 2; i++) {
+				int targetX, targetY;
+				targetX = posX;
+				targetY = posY - i;
+				if (targetX >= 0 && targetX < 8 && targetY >= 0 && targetY < 8) {
+					movableX.push_back(targetX);
+					movableY.push_back(targetY);
+				}
+			}
+		}
 		// todo
 		// 注意吃法與移動法不同
 		// 注意兵過路吃
@@ -56,7 +141,6 @@ void ChessPiece::checkMovable() {
 	default:
 		break;
 	}
-
 }
 
 bool ChessPiece::move(int x, int y) {
