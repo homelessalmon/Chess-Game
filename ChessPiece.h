6e35ab15e0ed01c3738230f3ff3ea52852111259
homelessalmon <83202278+homelessalmon@users.noreply.github.com>
2021-05-17 03:48:29 +0800

    Update ChessPiece.h

--- ChessPiece.h
+++ ChessPiece.h
@@ -19,12 +19,15 @@ public:
 	int posY;
 	int player; // 0 = 白 1 = 黑
 	int moved = 0;
+	int epc = 0; //whether en passant capture is available
 	Type type;
 
 	vector<int> movableX;
 	vector<int> movableY;
 	vector<int> capturableX;
 	vector<int> capturableY;
+	vector<int> epcX;
+	vector<int> epcY;
 
 	//void checkMovable();
 	//bool move(int x, int y);
