7fe26d85f445cc12e61ce78a65aeacf707a1e628
homelessalmon <83202278+homelessalmon@users.noreply.github.com>
2021-05-17 07:42:24 +0800

    Update Board.h

--- Board.h
+++ Board.h
@@ -10,6 +10,7 @@ public:
 	void beCastling(ChessPiece& piece, int x);
 	void capture(Player& player, int i);
 	bool kingCheck(ChessPiece* const [8][8], ChessPiece&, int, int, int);
+	bool checkCheck(ChessPiece* const [8][8], ChessPiece&, int);
 	/*Board();
 	Board(Player* players[2]);*/
 };
