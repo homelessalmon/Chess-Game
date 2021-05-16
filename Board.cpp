fa8a11bb739a67c5170f3918eae98e29bf11a084
kaler8234 <61696147+kaler8234@users.noreply.github.com>
2021-05-16 12:11:56 +0800

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -1,1 +1,29 @@
 #include "Board.h"
+Board::Board()
+{
+	for (int i = 0; i < 8; i++)
+	{
+		for (int j = 0; j < 8; j++)
+		{
+			boardSituation[i][j] = NULL;
+		}
+	}
+}
+
+Board::Board(Player* players[2])
+{
+	for (int i = 0; i < 8; i++)
+	{
+		for (int j = 0; j < 8; j++)
+		{
+			boardSituation[i][j] = NULL;
+		}
+	}
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 0; j < players[i]->OwningPiece.size(); j++)
+		{
+			boardSituation[players[i]->OwningPiece[j].posY][players[i]->OwningPiece[j].posX] = &players[i]->OwningPiece[j];
+		}
+	}
+}
