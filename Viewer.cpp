f240200d2ebb9b489de1d92ddd0bd2532d6b39bc
shinku2846 <shinku2846@gmail.com>
2021-05-17 00:55:15 +0800

    Update Viewer.cpp

--- Viewer.cpp
+++ Viewer.cpp
@@ -162,6 +162,6 @@ void Viewer::drawMovable(Board board, int x, int y) {
 	for (int i = 0; i < board.boardSituation[x][y]->capturableX.size(); i++) {
 		Point A = BoradtoImg(board.boardSituation[x][y]->capturableX[i], board.boardSituation[x][y]->capturableY[i]);
 		rectangle(Screen, A, Point(A.x + SIZE, A.y + SIZE), Scalar(31, 23, 176), -1);
-		drawChess(board.boardSituation[capturableX[i]][capturableY[i]]);
+		drawChess(*board.boardSituation[board.boardSituation[x][y]->capturableX[i]][board.boardSituation[x][y]->capturableY[i]]);
 	}
 }
