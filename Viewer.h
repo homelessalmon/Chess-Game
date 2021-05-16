0f5e01628a992d34d089ae1bc068fb27a0708d0a
shinku2846 <shinku2846@gmail.com>
2021-05-17 00:02:22 +0800

    Update Viewer.h

--- Viewer.h
+++ Viewer.h
@@ -1,10 +1,10 @@
 #pragma once
 #include <opencv2/opencv.hpp>
 #include "ChessPiece.h"
+#include "Board.h"
 using namespace std;
 using namespace cv;
 typedef class ChessPiece ChessPiece;
-typedef class Viewer Viewer;
 #define SIZE 50
 
 class Viewer {
@@ -12,7 +12,7 @@ public:
 	Viewer();
 	void drawBoard(); //繪製棋盤、底板
 	void drawChess(ChessPiece piece); //繪製piece對應的棋子
-	void drawMovable(ChessPiece piece); //繪製piece可走的方格
+	void drawMovable(Board board, int x, int y); //繪製piece可走的方格
 	Mat Screen;
 private:
 	Point BoradtoImg(int x, int y); //得出某棋盤格左上角的座標
