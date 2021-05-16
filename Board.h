b95fb646c947a7d3becaaaecc1aeebd540d41c33
shinku2846 <shinku2846@gmail.com>
2021-05-17 02:44:22 +0800

    Update Board.h

--- Board.h
+++ Board.h
@@ -6,9 +6,9 @@ class Board {
 public:
 	ChessPiece* boardSituation [8][8];
 	void checkMovable(ChessPiece &piece);
-	bool move(ChessPiece &piece, int x, int y, Player**);
+	bool move(ChessPiece &piece, int x, int y, Player** players);
 	void beCastling(ChessPiece &piece, int x);
-	void capture(Player&, int);
+	void capture(Player &player, int i);
 	/*Board();
 	Board(Player* players[2]);*/
 };
