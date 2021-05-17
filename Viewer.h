269fc63fe1f71929e6fb8d2fa169b3218d2568ae
shinku2846 <shinku2846@gmail.com>
2021-05-17 12:05:28 +0800

    Update Viewer.h

--- Viewer.h
+++ Viewer.h
@@ -12,12 +12,14 @@ public:
 	Viewer();
 	void drawMenu();
 	void drawBoard(); //繪製棋盤、底板
-	void drawButton(int undo, int redo);
+	void drawButton(int undo, int redo, int FF);
 	void drawTurn(int player);
 	void drawChess(ChessPiece piece); //繪製piece對應的棋子
 	void drawMovable(Board board, int x, int y); //繪製piece可走的方格
 	void drawPromotingTips(int player);
 	void drawCheck(ChessPiece piece);
+	void drawCheckmate(int loser);
+	void drawStalemate(int loser);
 	Mat Screen;
 private:
 	Point BoradtoImg(int x, int y); //得出某棋盤格左上角的座標
