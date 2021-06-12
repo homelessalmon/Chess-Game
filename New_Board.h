bc6b7ca5327eab3c141090a09dde48c2998666bf
kaler8234 <61696147+kaler8234@users.noreply.github.com>
2021-06-12 23:27:41 +0800

    Update New_Board.h

--- New_Board.h
+++ New_Board.h
@@ -7,22 +7,23 @@
 #include"ChessPiece.h"
 const string split = "-------------------------------------------------------------------";
 
-class New_Board
+class Board
 {
 public:
-	static vector<New_Board> board_history;
-	static std::stack<New_Board> stack;
-	ChessPiece* board[8][8];
+	static Board board;
+	static vector<Board> board_history;
+	static std::stack<Board> stack;
+	ChessPiece* boardSituation[8][8];
 	
-	New_Board();
-	New_Board(const New_Board& B);
-	~New_Board();
-	void operator=(New_Board);
-	bool operator==(New_Board);
+	Board();
+	Board(const Board& B);
+	~Board();
+	void operator=(Board);
+	bool operator==(Board);
 	static void load_board();
 	static void write_board();
 	static void write_init_board();
-	static New_Board return_now_board();
+	static Board return_now_board();
 	static vector<ChessPiece> return_chess_vector(int _player);
 	static void undo();
 	static void redo();
