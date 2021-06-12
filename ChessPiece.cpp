f9bce6c22f9dd2cdaaf975b1cacdb6d7a0efd0cc
shinku2846 <shinku2846@gmail.com>
2021-06-13 00:41:13 +0800

    Update ChessPiece.cpp

--- ChessPiece.cpp
+++ ChessPiece.cpp
@@ -11,3 +11,13 @@ ChessPiece::ChessPiece(int player, Type type, int posX, int posY) {
 	ChessPiece::posX = posX;
 	ChessPiece::posY = posY;
 }
+
+ChessPiece::ChessPiece(int _player, Type _type, int _posX, int _posY, int _epc, int _epcd, int _moved) {
+	player = _player;
+	type = _type;
+	posX = _posX;
+	posY = _posY;
+	epc = _epc;
+	epcd = _epcd;
+	moved = _moved;
+}
