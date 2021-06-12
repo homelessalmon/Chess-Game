8c56be16ea1ff9911f4920bf2ae727208c6e0497
shinku2846 <shinku2846@gmail.com>
2021-06-13 00:40:48 +0800

    Update Player.h

--- Player.h
+++ Player.h
@@ -1,19 +1,25 @@
 #pragma once
 #include <vector>
 #include "ChessPiece.h"
-#include<string>
-
+#include "Board.h"
 using namespace std;
-
 typedef class ChessPiece ChessPiece;
+typedef class Player player;
 
 class Player {
 public:
+
 	vector<ChessPiece> OwningPiece;
 	Player(int);
+	//virtual void onMove();
+	bool move(ChessPiece& piece, int x, int y, Player**);
+	void beCastling(ChessPiece& piece, int x);
+	void capture(Player& player, int i);
+	void promote(ChessPiece& piece, Type type);
 };
 
 class AI_player : public Player
 {
 	AI_player(int);
 };
+
