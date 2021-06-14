cc4d9bdb7db09b16cfea8608aa39f30147213b52
homelessalmon <83202278+homelessalmon@users.noreply.github.com>
2021-06-14 22:08:06 +0800

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -20,7 +20,7 @@ Player::Player(int n) { //n=0 white,1 black
 		OwningPiece.push_back(K);
 	}
 }
-HumanPlayer::HumanPlayer(int n) :Player(n) {}
+HumanPlayer::HumanPlayer(int n) :Player(n) {};
 AIPlayer::AIPlayer(int n) :Player(n) {}
 
 void Player::beCastling(ChessPiece& piece, int x) {
@@ -109,4 +109,115 @@ bool HumanPlayer::move(ChessPiece& piece, int x, int y, Player** players) {
 
 void HumanPlayer::promote(ChessPiece& piece, Type type) {
 	piece.type = type;
+}
+
+ChessPiece AIPlayer::choosePiece()
+{
+	vector<int>available;
+	for (int i = 0; i < OwningPiece.size(); i++) {
+		Board::board.checkMovable(OwningPiece[i]);
+		if (OwningPiece[i].capturableX.size() != 0 || OwningPiece[i].capturableX.size() != 0) {
+			available.push_back(i);
+		}
+	}
+	int idx = rand() % available.size();
+	return OwningPiece[idx];
+}
+
+bool AIPlayer::move(ChessPiece& piece, int x, int y, Player** players) {
+	Board::board.checkMovable(piece);
+
+	int act, capPos, movePos;
+	act = rand() % 2 + 1; //1:move 2:capture
+	if (piece.capturableX.size() == 0)
+		act = 1;
+
+	switch (act) {
+	case 1:
+		movePos = rand() % piece.movableX.size();
+		x = piece.movableX[movePos];
+		y = piece.movableY[movePos];
+		Board::board.boardSituation[x][y] = Board::board.boardSituation[piece.posX][piece.posY];
+		Board::board.boardSituation[piece.posX][piece.posY] = nullptr;
+		piece.posX = x;
+		piece.posY = y;
+		piece.moved++;
+		//epc
+		if (piece.type == Pawn && piece.player == 0) {
+			for (int i = 0; i < piece.epcX.size(); i++) {
+				if (piece.epcX[i] == x && piece.epcY[i] == y - 1) {
+					int idx;
+					for (idx = 0; idx < players[Board::board.boardSituation[x][y - 1]->player]->OwningPiece.size(); idx++) {
+						if (players[Board::board.boardSituation[x][y - 1]->player]->OwningPiece[idx].posX == x && players[Board::board.boardSituation[x][y - 1]->player]->OwningPiece[idx].posY == y - 1) {
+							break;
+						}
+					}
+					piece.epcd = 1;
+					capture(*players[Board::board.boardSituation[x][y - 1]->player], idx);
+				}
+			}
+		}
+		if (piece.type == Pawn && piece.player == 1) {
+			for (int i = 0; i < piece.epcX.size(); i++) {
+				if (piece.epcX[i] == x && piece.epcY[i] == y + 1) {
+					int idx;
+					for (idx = 0; idx < players[Board::board.boardSituation[x][y + 1]->player]->OwningPiece.size(); idx++) {
+						if (players[Board::board.boardSituation[x][y + 1]->player]->OwningPiece[idx].posX == x && players[Board::board.boardSituation[x][y + 1]->player]->OwningPiece[idx].posY == y + 1) {
+							break;
+						}
+					}
+					piece.epcd = 1;
+					capture(*players[Board::board.boardSituation[x][y + 1]->player], idx);
+				}
+			}
+		}
+
+		for (int j = 0; j < players[piece.player]->OwningPiece.size(); j++) {
+			players[piece.player]->OwningPiece[j].epc = 0;
+		}
+		if (piece.type == Pawn && piece.moved == 1)
+			piece.epc = 1;
+		return true;
+	case 2:
+		capPos = rand() % piece.capturableX.size();
+		x = piece.capturableX[capPos];
+		y = piece.capturableY[capPos];
+		int idx;
+		for (idx = 0; idx < players[Board::board.boardSituation[x][y]->player]->OwningPiece.size(); idx++) {
+			if (players[Board::board.boardSituation[x][y]->player]->OwningPiece[idx].posX == x && players[Board::board.boardSituation[x][y]->player]->OwningPiece[idx].posY == y) {
+				break;
+			}
+		}
+		capture(*players[Board::board.boardSituation[x][y]->player], idx);
+
+		Board::board.boardSituation[x][y] = Board::board.boardSituation[piece.posX][piece.posY];
+		Board::board.boardSituation[piece.posX][piece.posY] = nullptr;
+		piece.posX = x;
+		piece.posY = y;
+		piece.moved++;
+		for (int j = 0; j < players[piece.player]->OwningPiece.size(); j++) {
+			players[piece.player]->OwningPiece[j].epc = 0;
+		}
+		return true;
+	}
+	piece.epcd = 0;
+	return false;
+}
+
+void AIPlayer::promote(ChessPiece& piece, Type type) {
+	int r = rand() % 4;
+	switch (r) {
+	case 0:
+		piece.type = Queen;
+		break;
+	case 1:
+		piece.type = Rook;
+		break;
+	case 2:
+		piece.type = Bishop;
+		break;
+	case 3:
+		piece.type = Knight;
+		break;
+	}
 }
