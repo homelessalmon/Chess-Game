79dacfe5dbf82c8a9062ee14434246bcc796e2f1
shinku2846 <shinku2846@gmail.com>
2021-06-15 22:32:50 +0800

    Update Viewer.h

--- Viewer.h
+++ Viewer.h
@@ -19,6 +19,8 @@ public:
 	void drawMovable(Board board, int x, int y); //繪製piece可走的方格
 	void drawPromotingTips(int player);
 	void drawCheck(ChessPiece piece);
+	void drawCheckmateAnimation(double i, int loser);
+	void drawStalemateAnimation(double i, int loser);
 	void drawCheckmate(int loser);
 	void drawStalemate(int loser);
 	Mat Screen;
