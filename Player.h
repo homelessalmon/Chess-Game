19fdaef6446fe81a09c854d429b38ec949ed8204
kaler8234 <61696147+kaler8234@users.noreply.github.com>
2021-06-15 16:35:40 +0800

    Update Player.h

--- Player.h
+++ Player.h
@@ -7,29 +7,29 @@
 using namespace std;
 
 class Player {
-
 public:
 	vector<ChessPiece> OwningPiece;
 	Player(int);
 	virtual bool move(ChessPiece& piece, int x, int y, Player**) = 0;
 	virtual void promote(ChessPiece& piece, Type type) = 0;
+	virtual int return_player_type() = 0;
 	void beCastling(ChessPiece& piece, int x);
 	void capture(Player& player, int i);
 };
 
 class HumanPlayer : public Player {
-
 public:
 	HumanPlayer(int);
+	int return_player_type() { return 0; }
 	bool move(ChessPiece& piece, int x, int y, Player**);
 	void promote(ChessPiece& piece, Type type);
 };
 
 class AIPlayer : public Player {
-
 public:
 	AIPlayer(int);
 	ChessPiece& choosePiece();
+	int return_player_type() { return 1; }
 	bool move(ChessPiece& piece, int x, int y, Player**);
 	void promote(ChessPiece& piece, Type type);
 };
