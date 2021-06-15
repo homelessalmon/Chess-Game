8df8afb7623f1b6d7cc62dc68476f6fde1955007
shinku2846 <shinku2846@gmail.com>
2021-06-15 19:56:12 +0800

    Update GameManager.cpp

--- GameManager.cpp
+++ GameManager.cpp
@@ -51,7 +51,12 @@ void GameManager::drawAll() {
 		}
 	}
 	viewer.drawTurn(currentPlayer);
-	viewer.drawButton(1, 1, 1);
+	if (status == Replaying) {
+		viewer.drawButton(1, 1);
+	}
+	else {
+		viewer.drawButton(0, 1);
+	}
 	imshow("Chess Game", viewer.Screen);
 }
 
@@ -64,14 +69,11 @@ void GameManager::done() {
 	renewBoard();
 	Board::board_history.push_back(Board::board);
 	check = !Board::board.checkCheck(currentPlayer);
-	int opponent;
 	if (currentPlayer == 0) {
 		currentPlayer = 1;
-		opponent = 0;
 	}
 	else {
 		currentPlayer = 0;
-		opponent = 1;
 	}
 	drawAll();
 }
@@ -80,25 +82,89 @@ void GameManager::doMouseCallbackPromoting(int event, int x, int y, int flags) {
 	if (event == EVENT_LBUTTONUP) {
 		if (x >= SIZE * 2.8125 && x <= SIZE * 3.8125 && y >= SIZE * 5 && y <= SIZE * 6) {
 			players[currentPlayer]->promote(players[currentPlayer]->OwningPiece[pieceNo], Queen);
-			logTemp = logTemp + " " + 'Q';
 		}
 		else if (x >= SIZE * 3.9375 && x <= SIZE * 4.9375 && y >= SIZE * 5 && y <= SIZE * 6) {
 			players[currentPlayer]->promote(players[currentPlayer]->OwningPiece[pieceNo], Rook);
-			logTemp = logTemp + " " + 'R';
 		}
 		else if (x >= SIZE * 5.0625 && x <= SIZE * 6.0625 && y >= SIZE * 5 && y <= SIZE * 6) {
 			players[currentPlayer]->promote(players[currentPlayer]->OwningPiece[pieceNo], Bishop);
-			logTemp = logTemp + " " + 'B';
 		}
 		else if (x >= SIZE * 6.1875 && x <= SIZE * 7.1875 && y >= SIZE * 5 && y <= SIZE * 6) {
 			players[currentPlayer]->promote(players[currentPlayer]->OwningPiece[pieceNo], Knight);
-			logTemp = logTemp + " " + 'K';
 		}
 		else {
 			return;
 		}
 		status = Standby;
 		done();
+	}
+}
+
+void GameManager::doMouseCallbackReplaying(int event, int x, int y, int flags) {
+	if (event == EVENT_LBUTTONUP) {
+		Point PointStart = Point(x, y);
+		int clickedX = GameManager::ImgtoBoradX(PointStart);
+		int clickedY = GameManager::ImgtoBoradY(PointStart);
+		if (PointStart.x >= SIZE * 9.125 && PointStart.x <= SIZE * 9.875 && PointStart.y >= SIZE * 1.5 && PointStart.y <= SIZE * 2) {
+			if (Viewer::plateFace == 0) {
+				Viewer::plateFace = 1;
+			}
+			else {
+				Viewer::plateFace = 0;
+			}
+			drawAll();
+		}
+		else if (PointStart.x >= SIZE * 9.125 && PointStart.x <= SIZE * 9.875 && PointStart.y >= SIZE * 2.5 && PointStart.y <= SIZE * 3) {
+			//undo
+			if (Board::undo()) {
+				players[0]->OwningPiece = Board::return_chess_vector(0);
+				players[1]->OwningPiece = Board::return_chess_vector(1);
+				viewer.drawBoard();
+				for (int j = 0; j < 2; j++) {
+					for (int i = 0; i < players[j]->OwningPiece.size(); i++) {
+						viewer.drawChess(players[j]->OwningPiece[i]);
+					}
+				}
+				viewer.drawButton(1, 1);
+				if (currentPlayer == 0) {
+					currentPlayer = 1;
+				}
+				else {
+					currentPlayer = 0;
+				}
+				viewer.drawTurn(currentPlayer);
+				imshow("Chess Game", viewer.Screen);
+			}
+		}
+		else if (PointStart.x >= SIZE * 9.125 && PointStart.x <= SIZE * 9.875 && PointStart.y >= SIZE * 3.5 && PointStart.y <= SIZE * 4) {
+			//redo
+			if (Board::redo()) {
+				players[0]->OwningPiece = Board::return_chess_vector(0);
+				players[1]->OwningPiece = Board::return_chess_vector(1);
+				viewer.drawBoard();
+				for (int j = 0; j < 2; j++) {
+					for (int i = 0; i < players[j]->OwningPiece.size(); i++) {
+						viewer.drawChess(players[j]->OwningPiece[i]);
+					}
+				}
+				viewer.drawButton(1, 1);
+				if (currentPlayer == 0) {
+					currentPlayer = 1;
+				}
+				else {
+					currentPlayer = 0;
+				}
+				viewer.drawTurn(currentPlayer);
+				imshow("Chess Game", viewer.Screen);
+
+			}
+		}
+		else if (PointStart.x >= SIZE * 9.125 && PointStart.x <= SIZE * 9.875 && PointStart.y >= SIZE * 6.5 && PointStart.y <= SIZE * 7) {
+			//FF.
+			viewer.drawButton(1, 1);
+			imshow("Chess Game", viewer.Screen);
+			status = Checkmate;
+		}
 	}
 }
 
@@ -109,20 +175,21 @@ void GameManager::mouseCallbackPromoting(int event, int x, int y, int flags, voi
 	self->doMouseCallbackPromoting(event, x, y, flags);
 }
 
+void GameManager::mouseCallbackReplaying(int event, int x, int y, int flags, void* param) {
+	GameManager* self = static_cast<GameManager*>(param);
+	self->doMouseCallbackReplaying(event, x, y, flags);
+}
+
 void GameManager::doMouseCallbackMoving(int event, int x, int y, int flags) {
 	if (event == EVENT_LBUTTONUP) {
 		Point PointStart = Point(x, y);
 		int clickedX = GameManager::ImgtoBoradX(PointStart);
 		int clickedY = GameManager::ImgtoBoradY(PointStart);
 		bool flag;
-		logTemp = "";
-		char p = currentPlayer + '0';
-		logTemp = logTemp + p + " ";
 		if (players[currentPlayer]->OwningPiece[pieceNo].type == King && players[currentPlayer]->OwningPiece[pieceNo].posX - clickedX == -2) {
 			flag = players[currentPlayer]->move(players[currentPlayer]->OwningPiece[pieceNo], clickedX, clickedY, players);
 			if (flag == true) {
 				players[currentPlayer]->beCastling(*Board::board.boardSituation[players[currentPlayer]->OwningPiece[pieceNo].posX + 1][players[currentPlayer]->OwningPiece[pieceNo].posY], clickedX - 1);
-				logTemp = logTemp + "O-O";
 			}
 
 
@@ -131,40 +198,15 @@ void GameManager::doMouseCallbackMoving(int event, int x, int y, int flags) {
 			flag = players[currentPlayer]->move(players[currentPlayer]->OwningPiece[pieceNo], clickedX, clickedY, players);
 			if (flag == true) {
 				players[currentPlayer]->beCastling(*Board::board.boardSituation[players[currentPlayer]->OwningPiece[pieceNo].posX - 2][players[currentPlayer]->OwningPiece[pieceNo].posY], clickedX + 1);
-				logTemp = logTemp + "O-O-O";
 			}
 
 		}
 		else {
-			char captured = 'X';
-			if (Board::board.boardSituation[clickedX][clickedY] != nullptr) {
-				switch (Board::board.boardSituation[clickedX][clickedY]->type) {
-				case Queen:
-					captured = 'Q';
-					break;
-				case Bishop:
-					captured = 'B';
-					break;
-				case Knight:
-					captured = 'K';
-					break;
-				case Rook:
-					captured = 'R';
-					break;
-				case Pawn:
-					captured = 'P';
-					break;
-				default:
-					break;
-				}
-			}
 			flag = players[currentPlayer]->move(players[currentPlayer]->OwningPiece[pieceNo], clickedX, clickedY, players);
 			if (flag) {
 				char endX = clickedX + '0';
 				char endY = clickedY + '0';
-				logTemp = logTemp + startX + " " + startY + " " + endX + " " + endY + " " + captured;
 				if (players[currentPlayer]->OwningPiece[pieceNo].epcd == 1) {
-					logTemp = logTemp + " " + 'P';
 					players[currentPlayer]->OwningPiece[pieceNo].epcd = 0;
 				}
 			}
@@ -198,7 +240,7 @@ void GameManager::doMouseCallbackMoving(int event, int x, int y, int flags) {
 				}
 			}
 			viewer.drawTurn(currentPlayer);
-			viewer.drawButton(1, 1, 1);
+			viewer.drawButton(0, 1);
 		}
 		imshow("Chess Game", viewer.Screen);
 	}
@@ -247,7 +289,7 @@ void GameManager::doMouseCallbackStandby(int event, int x, int y, int flags) {
 						viewer.drawChess(players[j]->OwningPiece[i]);
 					}
 				}
-				viewer.drawButton(1, 1, 1);
+				viewer.drawButton(0, 1);
 				if (currentPlayer == 0) {
 					currentPlayer = 1;
 				}
@@ -269,7 +311,7 @@ void GameManager::doMouseCallbackStandby(int event, int x, int y, int flags) {
 						viewer.drawChess(players[j]->OwningPiece[i]);
 					}
 				}
-				viewer.drawButton(1, 1, 1);
+				viewer.drawButton(0, 1);
 				if (currentPlayer == 0) {
 					currentPlayer = 1;
 				}
@@ -283,9 +325,15 @@ void GameManager::doMouseCallbackStandby(int event, int x, int y, int flags) {
 		}
 		else if (PointStart.x >= SIZE * 9.125 && PointStart.x <= SIZE * 9.875 && PointStart.y >= SIZE * 6.5 && PointStart.y <= SIZE * 7) {
 			//FF.
-			viewer.drawButton(1, 1, 0);
+			viewer.drawButton(0, 1);
 			imshow("Chess Game", viewer.Screen);
 			status = Checkmate;
+		}
+		else if (PointStart.x >= SIZE * 9.125 && PointStart.x <= SIZE * 9.875 && PointStart.y >= SIZE * 8.5 && PointStart.y <= SIZE * 9) {
+			cout << "Please Enter File's Name: " << endl;
+			cin >> filename;
+			filename = "save/" + filename + ".txt";
+			status = End;
 		}
 	}
 }
@@ -333,35 +381,29 @@ void GameManager::mouseCallbackMenu(int event, int x, int y, int flags, void* pa
 }
 
 void GameManager::exe() {
-	
-	Board::board = Board::return_now_board();
+	system("cls");
 	namedWindow("Chess Game", WINDOW_AUTOSIZE);
 	viewer.drawMenu();
 	imshow("Chess Game", viewer.Screen);
-	system("cls");
 	while (1) {
 		setMouseCallback("Chess Game", mouseCallbackMenu, this);
-		waitKey(100);
+		cv::waitKey(100);
 		if (status != Menu) {
 			break;
 		}
 	}
 	if (status == NewGame) {
-		int player0, player1;
 		system("cls");
 		cout << "Please Enter White Player's Status. 0 = Human, 1 = AI" << endl;
-		cin >> player0;
+		cin >> playerAI[0];
 		cout << "Please Enter Black Player's Status. 0 = Human, 1 = AI" << endl;
-		cin >> player1;
-		if (player0 == 1 && player1 == 0) {
-			Viewer::plateFace = 1;
-		}
-		else {
-			Viewer::plateFace = 0;
-		}
+		cin >> playerAI[1];
+
 		system("cls");
+		Board::board = Board::return_now_board();
 		Board::write_init_board();
-		Board::load_board();
+		int non[4] = { 0 };
+		Board::load_board(non[0], non[1], non[2], non[3]);
 
 		viewer.drawBoard();
 		for (int i = 0; i < 2; i++) {
@@ -373,9 +415,8 @@ void GameManager::exe() {
 				viewer.drawChess(players[j]->OwningPiece[i]);
 			}
 		}
-		int undoable = 1, redoable = 1;
 
-		viewer.drawButton(undoable, redoable, 1);
+		viewer.drawButton(0, 1);
 		currentPlayer = 0;
 		viewer.drawTurn(currentPlayer);
 		imshow("Chess Game", viewer.Screen);
@@ -384,21 +425,55 @@ void GameManager::exe() {
 	}
 	else if (status == Continue) {
 		//todo
-		cout << "File's Name. :";
+		cout << "File's Name. :" << endl;
 		cin >> filename;
-		status = Standby;
+		filename = "save/" + filename + ".txt";
+		if (Board::specific_load_board(filename, playerAI[0], tick[0], playerAI[1], tick[1])) {
+			Board::board = Board::return_now_board();
+			players[0]->OwningPiece = Board::return_chess_vector(0);
+			players[1]->OwningPiece = Board::return_chess_vector(1);
+			status = Standby;
+			if (Board::board_history.size() % 2) {
+				currentPlayer = 0;
+			}
+			else {
+				currentPlayer = 1;
+			}
+		}
+		else {
+			cout << "Can't Open File." << endl;
+		}
 	}
 	else if (status == Replay) {
 		//todo
-		cout << "File's Name. :";
+		cout << "File's Name. :" << endl;
 		cin >> filename;
-		status = Replaying;
+		filename = "replay/" + filename + ".txt";
+		if (Board::specific_load_board(filename, playerAI[0], tick[0], playerAI[1], tick[1])) {
+			Board::board = Board::return_now_board();
+			players[0]->OwningPiece = Board::return_chess_vector(0);
+			players[1]->OwningPiece = Board::return_chess_vector(1);
+			while (Board::undo()) {
+				players[0]->OwningPiece = Board::return_chess_vector(0);
+				players[1]->OwningPiece = Board::return_chess_vector(1);
+			}
+			status = Replaying;
+			currentPlayer = 0;
+		}
+		else {
+			cout << "Can't Open File." << endl;
+		}
 	}
 	else if (status == End) {
 		exit(0);
 	}
+	if (playerAI[0] == 1 && playerAI[0] == 0) {
+		Viewer::plateFace = 1;
+	}
+	else {
+		Viewer::plateFace = 0;
+	}
 	while (status == Standby || status == Moving || status == Promoting) {
-
 		switch (status) {
 		case Standby:
 			setMouseCallback("Chess Game", mouseCallbackStandby, this);
@@ -412,21 +487,22 @@ void GameManager::exe() {
 		default:
 			break;
 		}
-		waitKey(100);
+		cv::waitKey(100);
 		tick[currentPlayer]++;
 		cout << "White: " << tick[0] / 600 << ":" << (tick[0] % 600) / 10 << " | " << "Black: " << tick[1] / 600 << ":" << (tick[1] % 600) / 10 << '\r';
 		if (tick[currentPlayer] == 6000) {
 			status = Checkmate;
-			viewer.drawButton(1, 1, 0);
+			viewer.drawButton(0, 0);
 			imshow("Chess Game", viewer.Screen);
 		}
 
 	}
 	while (status == Replaying) {
 		//todo
+		setMouseCallback("Chess Game", mouseCallbackReplaying, this);
 	}
 	setMouseCallback("Chess Game", mouseCallbackEnd, this);
-	waitKey(3000);
+	cv::waitKey(3000);
 	while (1) {
 		switch (status) {
 		case Checkmate:
@@ -440,11 +516,11 @@ void GameManager::exe() {
 			setMouseCallback("Chess Game", mouseCallbackEnd, this);
 			break;
 		case End:
-			exit(0);
+			return;
 		default:
 			break;
 		}
-		waitKey(100);
+		cv::waitKey(100);
 	}
 }
 
