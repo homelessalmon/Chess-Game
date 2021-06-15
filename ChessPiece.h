6343cb7666bfbb2c2b48778c83bf83fa2a00bc6e
shinku2846 <shinku2846@gmail.com>
2021-06-16 01:35:39 +0800

    Update ChessPiece.h

--- ChessPiece.h
+++ ChessPiece.h
@@ -15,13 +15,12 @@ enum Type {
 class ChessPiece {
 public:
 	ChessPiece(int player, Type type, int posX, int posY);
-	ChessPiece(int _player, Type _type, int _posX, int _posY, int _epc, int _epcd, int _moved);
+	ChessPiece(int _player, Type _type, int _posX, int _posY, int _epc, int _moved);
 	int posX;
 	int posY;
 	int player; // 0 = 白 1 = 黑
 	int moved = 0;
 	int epc = 0; //whether en passant capture is available
-	int epcd = 0;
 	Type type;
 
 	vector<int> movableX;
